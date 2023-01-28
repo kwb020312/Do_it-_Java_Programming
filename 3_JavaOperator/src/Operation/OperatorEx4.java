2a84420c25a1bac4e8063329c16ea1b5fa70879f
N S <kwb020312@naver.com>
2023-01-28 17:09:40 +0900

    자바의 연산자에 대해 학습하고 정리했습니다.

---
+++ 3_JavaOperator/src/Operation/OperatorEx4.java
@@ -0,0 +1,15 @@
+package Operation;
+
+public class OperatorEx4 {
+
+	public static void main(String[] args) {
+		int fatherAge = 50;
+		int motherAge = 46;
+		
+		char older = fatherAge > motherAge ? 'F' : 'M' ;
+		
+		System.out.println(older); // print 'F'
+
+	}
+
+}
---
+++ 3_JavaOperator/src/Operation/OperatorEx5.java
@@ -0,0 +1,11 @@
+package Operation;
+
+public class OperatorEx5 {
+	public static void main(String[] args) {
+		int a = 5;
+		
+		System.out.println(a << 2); // print 20
+		System.out.println(a >> 2);	// print 1
+		System.out.println(a >>> 2); // print 1
+	}
+}
