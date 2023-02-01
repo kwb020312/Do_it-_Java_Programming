089d0a316160d0a1c9a8a7a3bc8d04484edec6cb
N S <kwb020312@naver.com>
2023-02-01 23:20:18 +0900

    컬렉션 프레임워크가 무엇인지 알아보고 여러 자료형을 사용하는 제네릭과 순회를 위해 사용하는 이터레이터를 알아보는 등 심도있는 학습을 진행했습니다.

---
+++ 12_CollectionFrameWork/src/collection/LinkedListTest.java
@@ -0,0 +1,27 @@
+package collection;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		LinkedList<String> myList = new LinkedList<String>();
+		
+		myList.add("A");
+		myList.add("B");
+		myList.add("C");
+		
+		System.out.println(myList);
+		
+		myList.add(1, "D");
+		
+		System.out.println(myList);
+		
+		myList.addFirst("0");
+		System.out.println(myList);
+		
+		myList.removeLast();
+		System.out.println(myList);
+	}
+
+}
---
+++ 12_CollectionFrameWork/src/generics/Powder.java
@@ -0,0 +1,10 @@
+package generics;
+
+public class Powder extends Material {
+	public void doPrinting() {
+		System.out.println("Powder 재료로 출력합니다.");
+	}
+	public String toString() {
+		return "재료는 Powder입니다.";
+	}
+}
