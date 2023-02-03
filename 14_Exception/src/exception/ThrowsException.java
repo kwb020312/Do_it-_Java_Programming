9a048badefe3ffadc9b7211493eb3afb4b91279d
N S <kwb020312@naver.com>
2023-02-03 22:48:55 +0900

    예외처리에 대해 학습하고, 사용자 정의 예외를 생성해 로그를 남겨보며 학습했습니다.

---
+++ 14_Exception/src/exception/ThrowsException.java
@@ -0,0 +1,26 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+	
+	public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException {
+		FileInputStream fis = new FileInputStream(fileName);
+		Class c = Class.forName(className);
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		ThrowsException test = new ThrowsException();
+		try {
+			test.loadClass("a.text", "java.lang.String");
+		} catch (FileNotFoundException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
