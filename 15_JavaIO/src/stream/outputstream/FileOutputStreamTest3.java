ca5cfa81fea7a4aee4cc64b4437ef92d9ded418f
N S <kwb020312@naver.com>
2023-02-04 13:17:27 +0900

    Java의 다양한 Input/Outer에 대해 학습하고, 직렬화 역직렬화를 배우며 파일을 생성/읽기 해보았습니다.

---
+++ 15_JavaIO/src/stream/outputstream/FileOutputStreamTest3.java
@@ -0,0 +1,22 @@
+package stream.outputstream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileOutputStreamTest3 {
+
+	public static void main(String[] args) {
+		try(FileOutputStream fos = new FileOutputStream("output3.txt", true)) {
+			byte[] bs = new byte[26];
+			byte data = 65;
+			for(int i = 0 ; i < bs.length ; i ++) {
+				bs[i] = data;
+				data++;
+			}
+			fos.write(bs, 2, 10);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("출력이 완료되었습니다.");
+	}
+}
---
+++ 15_JavaIO/src/stream/writer/FileWriterTest.java
@@ -0,0 +1,23 @@
+package stream.writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterTest {
+
+	public static void main(String[] args) {
+		try(FileWriter fw = new FileWriter("writer.txt")) {
+			fw.write('A');
+			char buf[] = {'B', 'C', 'D', 'E', 'F', 'G'};
+			
+			fw.write(buf);
+			fw.write("안녕하세요. 잘 써지네요");
+			fw.write(buf, 1, 2);
+			fw.write("65");
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("출력이 완료되었습니다.");
+	}
+
+}
